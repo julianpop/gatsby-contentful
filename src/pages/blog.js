c64fec6573a278c382868c3944ed235d3e41385f
Julian <julianpop@hotmail.com>
2020-09-05 11:34:09 -0500

    Update blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -10,6 +10,8 @@ const blog = () => (
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis viverra
         faucibus elementum. Nam eget magna enim.
       </p>
+      <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id sodales enim. Suspendisse convallis lorem sit amet nunc gravida gravida. Ut porta eros quis sem faucibus dignissim. Morbi mattis aliquet mauris, id ullamcorper neque aliquet ac.</p>
+      <p className={styles.text}>Consectetur adipiscing elit. Mauris id sodales enim. Suspendisse convallis lorem sit amet nunc gravida gravida. Ut porta eros quis sem faucibus dignissim. Morbi mattis aliquet mauris, id ullamcorper neque aliquet ac.</p>
     </div>
   </Layout>
 )
